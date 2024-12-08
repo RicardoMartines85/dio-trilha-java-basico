fc997d84e342c85379d1a090402f970b31a010c1
RicardoMartines85 <ricardo.martines@live.com>
2024-12-08 10:29:38 -0300

    Conta terminal finalizado!

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+    //TODO: conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    //Mensagem para os usuarios
+    // capturar os valores digitados
+       // variaveis
+       System.out.println("Digite seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua argencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite sua conta:");
+        String conta = scanner.next();
+
+        double saldo = 237.48;
+
+        String nomeInteiro = nome.concat(" ").concat(sobrenome);
+        
+    //saída, mensagens aos usuarios
+    // olá + nomeCliente , obrigado por criar uma conta em nosso banco, sua argencia é +argencia , conta + conta e seu saldo + saldo já esta disponivel para saque.
+        
+System.out.println("Olá " + nomeInteiro + " , obrigado por criar uma conta em nosso banco, sua argencia é " + agencia +", conta" + conta +" e seu saldo de " + saldo +" já está disponível para saque.");
+}
 }
